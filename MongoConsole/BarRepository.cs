6c006a0a906aba78ed5f41cd46efe852ff475094
Miguel Fernández <mikeloguay@gmail.com>
2025-06-28 18:20:30 +0200

    Mongo insert y select inicial

---
+++ MongoConsole/BarRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoConsole;
+
+public interface IBarRepository
+{
+    Task InsertOneAsync(BsonDocument document);
+    List<BsonDocument> Find(FilterDefinition<BsonDocument> filter);
+}
+
+public class BarRepository(IMongoDatabase database) : IBarRepository
+{
+    private readonly IMongoCollection<BsonDocument> _collection = database.GetCollection<BsonDocument>("bar");
+    
+    public async Task InsertOneAsync(BsonDocument document) => await _collection.InsertOneAsync(document);
+    public List<BsonDocument> Find(FilterDefinition<BsonDocument> filter) => _collection.Find(filter).ToList();
+}
---
+++ MongoConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoConsole;
+
+MongoClient client = new("mongodb://mongouser:password1@localhost:27017/?authSource=admin");
+IMongoDatabase database = client.GetDatabase("foo");
+BarRepository barRepository = new(database);
+
+await barRepository.InsertOneAsync(new BsonDocument("Name", "Pepe"));
+
+var list = barRepository.Find(new BsonDocument("Name", "Pepe"));
+
+foreach (var document in list)
+{
+    Console.WriteLine(document["Name"]);
+}
